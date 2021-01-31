7b47a3587ae6bc97fe6b98eeb63fa2ce4a284e68
Mohamed Ahamed <mohamedx.28@gmail.com>
2021-01-31 20:39:27 +0000

    merging master with dev

--- app/UserDiagnosis.php
+++ app/UserDiagnosis.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserDiagnosis extends Model
 {
-    //
+    protected $table = 'user_diagnosis';
 }
--- routes/api.php
+++ routes/api.php
@@ -18,13 +18,15 @@ const BASE = '/v1/user/';
 Route::get(BASE . 'home', 'HomeController@home');
 
 Route::group(['middleware' => 'auth.jwt'], function () {
-    Route::get(BASE . 'hello', 'UserController@sayHello');
+    Route::get(BASE . 'login', 'UserController@login');
 
     Route::get(BASE . 'search', 'SearchController@searchDoc');
     Route::get(BASE . 'searchLab', 'SearchController@searchLab');
 
     Route::post(BASE . 'reservDoc', 'ReservationsController@reservDoc');
     Route::post(BASE . 'reservLab', 'UserDiagnosisController@userDiagnosis');
+
+    Route::post(BASE . 'updateProfile', 'UserController@updateProfile');
 });
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
