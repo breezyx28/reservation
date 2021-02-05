6a2d2d4eb0e7a349c68d14b56bd41eebf215fc13
Mohamed Ahamed <mohamedx.28@gmail.com>
2021-02-05 20:38:07 +0000

    previous booking

--- app/Reservations.php
+++ app/Reservations.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Reservations extends Model
 {
     protected $table = 'reservations';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userID');
+    }
+
+    public function hospitalInfo()
+    {
+        return $this->belongsTo(hospitalInfo::class, 'hospitalInfoID');
+    }
+
+    public function getSevrvicesArrayAttribute($value)
+    {
+        return Services::find($value);
+    }
 }
--- app/UserDiagnosis.php
+++ app/UserDiagnosis.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class UserDiagnosis extends Model
 {
     protected $table = 'user_diagnosis';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userID');
+    }
+
+    public function lab()
+    {
+        return $this->belongsTo(Lab::class, 'labID');
+    }
+
+    public function labDiagnosis()
+    {
+        return $this->belongsTo(LabDiagnosis::class, 'labDiagnosisID');
+    }
 }
