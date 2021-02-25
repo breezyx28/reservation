f7ecda7003316d3d986b94989c2b2b7d9e0138e0
Mohamed Ahamed <mohamedx.28@gmail.com>
2021-02-25 16:54:05 +0000

    fix admin allhospitalServices return to original 1

--- app/Http/Controllers/HospitalServicesController.php
+++ app/Http/Controllers/HospitalServicesController.php
@@ -11,7 +11,7 @@ class HospitalServicesController extends Controller
     public function index()
     {
         try {
-            $data = \App\Services::with('hospital', 'services')->get();
+            $data = \App\HospitalServices::with('hospital', 'services')->get();
             return ResponseMessage::Success('تم بنجاح', $data);
         } catch (\Exception $e) {
             return ResponseMessage::Error('حدث خطأ ما', $e->getMessage());
